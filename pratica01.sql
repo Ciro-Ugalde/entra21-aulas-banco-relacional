70c8cdbf81a73da6a453594fd274da65e67c3e3f
Ciro-Ugalde <ciro.daniel.ugalde@gmail.com>
2022-06-23 12:04:06 -0300

    finalizando pratica aula01

---
+++ pratica01.sql
@@ -0,0 +1,11 @@
+--Visualizar praticantes
+select * from praticante; 
+
+--Insere um registro na tabela
+insert into praticante (name,age,modalidade) values("Ciro" ,33, "Basquete");
+insert into praticante (name,age,modalidade) values("Daniel" ,28, "Futebol");
+insert into praticante (name,age,modalidade) values("Juliano" ,39, "Ciclismo");
+insert into praticante (name,age,modalidade) values("Marcelo" ,22, "Volei");
+
+--Deletar registros da tabela
+delete from praticante;
